2334fb3a66cd71d28b5d91b35659aa8ad7d91db8
faizan <faizan.ahmad@srmtechsol.com>
2017-05-30 14:15:59 +0530

    first commit

---
+++ aspwithjenkins/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspwithjenkins.MvcApplication" Language="C#" %>
---
+++ aspwithjenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspwithjenkins.Startup))]
+namespace aspwithjenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
